15322a619f07beb6a3e7ca6bcded02948c4b4830
Bkind254 <langatbrian02@gmail.com>
2023-06-02 20:20:49 +0300

    2ND commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Navbar from "./components/Navbar";
+import Navbar from "./components/navbar";
 import Hero from "./pages/Hero";
 import Menu from "./pages/Menu";
 import Food from "./pages/Food";
